7341b9c4ab812bf05dfca2888ed6e31ce5522bd9
Orelsaada <orel.amedi@gmail.com>
2020-03-29 03:07:27 +0300

    nothing but test

--- index.py
+++ index.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    mainLoop()
-    columns_titles, all_columns_list, columns_number, rows_number = readCSV()
+    # mainLoop()
+    # columns_titles, all_columns_list, columns_number, rows_number = readCSV()
     # return render_template("index.html", columns_titles=columns_titles,
     #                        all_columns_list=all_columns_list,
     #                        columns_number=columns_number, rows_number=rows_number)
