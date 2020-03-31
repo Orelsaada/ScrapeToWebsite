0acee6428c29895da92b42cf7af0f0f1a2768d63
Orelsaada <orel.amedi@gmail.com>
2020-04-01 00:14:19 +0300

    test how web function without the csv file

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from ReadFromCSV.CsvReader import readCSV
-from main import mainLoop
+
 
 app = Flask(__name__)
 
@@ -11,7 +11,6 @@ def index():
     return render_template("index.html", columns_titles=columns_titles,
                            all_columns_list=all_columns_list,
                            columns_number=columns_number, rows_number=rows_number)
-    # return render_template('test.html')
 
 
 if __name__ == '__main__':
